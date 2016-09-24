74c3e68ac829deb872809051e89b6109dfbe00e0
Twibbit <dandantpm@hotmail.com>
2016-09-24 12:24:30 -0500

    Creating Default Sub Objects

--- BattleTank/Source/BattleTank/Public/Tank.h
+++ BattleTank/Source/BattleTank/Public/Tank.h
@@ -2,8 +2,10 @@
 
 #pragma once
 
+#include "TankAimingComponent.h"
 #include "GameFramework/Pawn.h"
 #include "Tank.generated.h"
+
 
 UCLASS()
 class BATTLETANK_API ATank : public APawn
@@ -14,6 +16,8 @@ public:
 
 	void AimAt(FVector HitLocation);
 
+protected:
+	UTankAimingComponent* TankAimingComponent = nullptr;
 private:
 	// Sets default values for this pawn's properties
 	ATank();
